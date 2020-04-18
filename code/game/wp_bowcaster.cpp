6cba67ff0c3b8af5cbebd4430ae747a09ee3857d
Max-Morten Conrad <max@kokoni.eu>
2020-04-18 10:33:30 +0200

    wp_bowcaster.cpp cleanup

--- code/game/wp_bowcaster.cpp
+++ code/game/wp_bowcaster.cpp
@@ -51,12 +51,6 @@ void WP_FireBowcasterMissile(gentity_t* ent, vec3_t start, vec3_t dir, qboolean 
 		}
 	}
 
-	// Testing: aim better? - It actually does help when zooming, need to split alt and main fire like the disruptor
-	//VectorCopy(ent->client->renderInfo.eyePoint, start);
-	//AngleVectors(ent->client->renderInfo.eyeAngles, dir, NULL, NULL);
-
-	//VectorCopy(ent->client->renderInfo.eyePoint, start); //Testing: does it hit correctly when zoomed in?
-
 	WP_TraceSetStart(ent, start, vec3_origin, vec3_origin);//make sure our start point isn't on the other side of a wall
 
 	WP_MissileTargetHint(ent, start, dir);
@@ -83,16 +77,6 @@ void WP_FireBowcasterMissile(gentity_t* ent, vec3_t start, vec3_t dir, qboolean 
 		}
 	}
 
-	//	if ( ent->client )
-	//	{
-	//		if ( ent->client->ps.powerups[PW_WEAPON_OVERCHARGE] > 0 && ent->client->ps.powerups[PW_WEAPON_OVERCHARGE] > cg.time )
-	//		{
-	//			// in overcharge mode, so doing double damage
-	//			missile->flags |= FL_OVERCHARGED;
-	//			damage *= 2;
-	//		}
-	//	}
-
 	missile->damage = damage;
 	missile->dflags = DAMAGE_DEATH_KNOCKBACK;
 	if (altFire)
@@ -118,6 +102,7 @@ void WP_BowcasterAltFire(gentity_t* ent, qboolean alt_fire)
 	AngleVectors(ent->client->renderInfo.eyeAngles, forwardVec, NULL, NULL);
 	WP_FireBowcasterMissile(ent, start, forwardVec, alt_fire);
 }
+
 //----------------------------------------------------------
 void WP_BowcasterMainFire(gentity_t *ent, qboolean alt_fire)
 //----------------------------------------------------------
@@ -138,58 +123,3 @@ void WP_FireBowcaster( gentity_t *ent, qboolean alt_fire )
 		WP_BowcasterMainFire( ent, alt_fire );
 	}
 }
-
-
-/*
-//---------------------------------------------------------
-void WP_FireBowcaster(gentity_t* ent, qboolean alt_fire)
-//---------------------------------------------------------
-{
-	vec3_t	dir, angs;
-
-	vectoangles(forwardVec, angs);
-
-    if (alt_fire)
-    {
-		return;
-    }
-
-	if (ent->client && ent->client->NPC_class == CLASS_VEHICLE)
-	{//no inherent aim screw up
-	}
-	else if (!(ent->client->ps.forcePowersActive & (1 << FP_SEE))
-		|| ent->client->ps.forcePowerLevel[FP_SEE] < FORCE_LEVEL_2)
-	{//force sight 2+ gives perfect aim
-		//FIXME: maybe force sight level 3 autoaims some?
-		if (alt_fire)
-		{
-			// add some slop to the alt-fire direction
-			angs[PITCH] += Q_flrand(-1.0f, 1.0f) * BOWCASTER_ALT_SPREAD;
-			angs[YAW] += Q_flrand(-1.0f, 1.0f) * BOWCASTER_ALT_SPREAD;
-		}
-		else
-		{
-			// Troopers use their aim values as well as the gun's inherent inaccuracy
-			// so check for all classes of stormtroopers and anyone else that has aim error
-			if (ent->client && ent->NPC &&
-				(ent->client->NPC_class == CLASS_STORMTROOPER ||
-					ent->client->NPC_class == CLASS_SWAMPTROOPER))
-			{
-				angs[PITCH] += (Q_flrand(-1.0f, 1.0f) * (BLASTER_NPC_SPREAD + (6 - ent->NPC->currentAim) * 0.25f));//was 0.5f
-				angs[YAW] += (Q_flrand(-1.0f, 1.0f) * (BLASTER_NPC_SPREAD + (6 - ent->NPC->currentAim) * 0.25f));//was 0.5f
-			}
-			else
-			{
-				// add some slop to the main-fire direction
-				angs[PITCH] += Q_flrand(-1.0f, 1.0f) * BOWCASTER_MAIN_SPREAD;
-				angs[YAW] += Q_flrand(-1.0f, 1.0f) * BOWCASTER_MAIN_SPREAD;
-			}
-		}
-	}
-
-	AngleVectors(angs, dir, NULL, NULL);
-
-	// FIXME: if temp_org does not have clear trace to inside the bbox, don't shoot!
-	WP_FireBowcasterMissile(ent, muzzle, dir, alt_fire);
-}
-*/
